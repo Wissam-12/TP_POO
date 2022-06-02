b4e6f1a1bdfccd57ea9d9e9eec9a7bd8f6e35f03
Sarahbr1 <ks_brahiti@esi.dz>
2022-06-02 15:34:55 +0100

    editable desactivée

--- src/main/java/com/example/tp/AcceuilView.java
+++ src/main/java/com/example/tp/AcceuilView.java
@@ -1,4 +1,24 @@
 package com.example.tp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AcceuilView {
+   private Stage stage;
+   private Scene scene;
+   private Parent root;
+
+   public void goTo (ActionEvent e) throws IOException {
+       root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+       stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
+   }
 }
